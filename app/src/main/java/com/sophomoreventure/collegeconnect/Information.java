55497dd29068c5745d3d4907dd8ec92dd944b04c
Vikas Kumar <vrnvikas1994@gmail.com>
2015-12-14 22:27:21 +0530

    added navigation drawer with recycler view

---
+++ app/src/main/java/com/sophomoreventure/collegeconnect/Information.java
@@ -0,0 +1,10 @@
+package com.sophomoreventure.collegeconnect;
+
+/**
+ * Created by Vikas Kumar on 14-12-2015.
+ */
+public class Information {
+    public int iconId;
+    public String title;
+}
+
---
+++ app/src/main/java/com/sophomoreventure/collegeconnect/MyApplication.java
@@ -0,0 +1,53 @@
+package com.sophomoreventure.collegeconnect;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by Vikas Kumar on 14-12-2015.
+ */
+public class MyApplication extends Application {
+
+    private static MyApplication sInstance;
+
+
+    public static MyApplication getInstance() {
+        return sInstance;
+    }
+
+    public static Context getAppContext() {
+        return sInstance.getApplicationContext();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sInstance = this;
+    }
+
+    public static void saveToPreferences(Context context, String preferenceName, String preferenceValue) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(preferenceName, preferenceValue);
+        editor.apply();
+    }
+
+    public static void saveToPreferences(Context context, String preferenceName, boolean preferenceValue) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(preferenceName, preferenceValue);
+        editor.apply();
+    }
+
+    public static String readFromPreferences(Context context, String preferenceName, String defaultValue) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+        return sharedPreferences.getString(preferenceName, defaultValue);
+    }
+
+    public static boolean readFromPreferences(Context context, String preferenceName, boolean defaultValue) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+        return sharedPreferences.getBoolean(preferenceName, defaultValue);
+    }
+}
