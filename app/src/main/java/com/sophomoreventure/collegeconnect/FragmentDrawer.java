45ecc26206e87a2617d7a21e97e4bc419be7c108
murali-prajapati <murali.prajapati555@gmail.com>
2015-12-14 23:04:59 +0530

    event layout added

--- app/src/main/java/com/sophomoreventure/collegeconnect/FragmentDrawer.java
+++ app/src/main/java/com/sophomoreventure/collegeconnect/FragmentDrawer.java
@@ -1,23 +1,20 @@
 package com.sophomoreventure.collegeconnect;
 
-import android.app.Activity;
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.sophomoreventure.collegeconnect.adapters.AdapterDrawer;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -166,6 +163,11 @@ public class FragmentDrawer extends android.support.v4.app.Fragment {
 
         @Override
         public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
         }
     }
 }
