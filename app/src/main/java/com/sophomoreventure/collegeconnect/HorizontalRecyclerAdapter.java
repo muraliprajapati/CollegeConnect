ae363e7a6c44daadc2f2886b3050157fc14244e5
murali-prajapati <murali.prajapati555@gmail.com>
2016-01-10 17:55:29 +0530

    HorizonalRecyclerView added

--- app/src/main/java/com/sophomoreventure/collegeconnect/HorizontalRecyclerAdapter.java
+++ app/src/main/java/com/sophomoreventure/collegeconnect/HorizontalRecyclerAdapter.java
@@ -5,12 +5,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
- * Created by Murali on 18/12/2015.
+ * Created by Murali on 10/01/2016.
  */
-class HorizontalRecyclerAdapter extends RecyclerView.Adapter<HorizontalRecyclerAdapter.ViewHolder> {
+public class HorizontalRecyclerAdapter extends RecyclerView.Adapter<HorizontalRecyclerAdapter.ViewHolder> {
 
     Context context;
 
@@ -32,26 +34,39 @@ class HorizontalRecyclerAdapter extends RecyclerView.Adapter<HorizontalRecyclerA
 
     @Override
     public void onBindViewHolder(HorizontalRecyclerAdapter.ViewHolder holder, int position) {
+        if (position % 2 == 0) {
+            holder.layout.setBackgroundResource(R.drawable.gradient_blue);
+        } else {
+            holder.layout.setBackgroundResource(R.drawable.gradient_orange);
+        }
 
+        holder.clubNameTextView.setText("IEEE");
     }
 
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 6;
     }
 
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        LinearLayout layout;
         TextView clubNameTextView;
 
 
         public ViewHolder(View itemView) {
             super(itemView);
             clubNameTextView = (TextView) itemView.findViewById(R.id.club_name_text_view);
+            layout = (LinearLayout) itemView.findViewById(R.id.single_column_linear_layout);
+            itemView.setOnClickListener(this);
 
         }
 
 
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(context, "Item clicked", Toast.LENGTH_SHORT).show();
+        }
     }
 }
---
+++ app/src/main/java/com/sophomoreventure/collegeconnect/MyEventsAdapter.java
@@ -0,0 +1,76 @@
+package com.sophomoreventure.collegeconnect;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Murali on 10/01/2016.
+ */
+class MyEventsAdapter extends RecyclerView.Adapter<MyEventsAdapter.ViewHolder> {
+
+    Context context;
+
+
+    public MyEventsAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public MyEventsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (parent instanceof RecyclerView) {
+            int layoutId = R.layout.event_card_view;
+            View view = LayoutInflater.from(parent.getContext()).inflate(layoutId, parent, false);
+            view.setFocusable(true);
+            return new ViewHolder(view);
+        } else {
+            throw new RuntimeException("Not bound to RecyclerView");
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(MyEventsAdapter.ViewHolder holder, int position) {
+
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+
+        return 6;
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        ImageView eventImageView;
+        TextView eventNameTextView;
+        TextView eventClubTextView;
+        TextView dateTextView;
+        CheckBox attendingCheckBox;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            eventImageView = (ImageView) itemView.findViewById(R.id.eventImageView);
+            eventNameTextView = (TextView) itemView.findViewById(R.id.eventNameTextView);
+            eventClubTextView = (TextView) itemView.findViewById(R.id.eventClubTextView);
+            dateTextView = (TextView) itemView.findViewById(R.id.eventDateTextView);
+            attendingCheckBox = (CheckBox) itemView.findViewById(R.id.attendingCheckBox);
+
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+
+        }
+    }
+}
