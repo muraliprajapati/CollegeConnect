dc2c4008ec3d65ceba8e62dae3d7143da581bd64
murali-prajapati <murali.prajapati555@gmail.com>
2016-01-21 19:19:52 +0530

    Backend API updated

--- app/src/main/java/com/sophomoreventure/collegeconnect/API.java
+++ app/src/main/java/com/sophomoreventure/collegeconnect/API.java
@@ -5,7 +5,10 @@ package com.sophomoreventure.collegeconnect;
  */
 public class API {
     public static final String USER_REG_API = "https://sheltered-fjord-8731.herokuapp.com/api/user/reg";
+    public static final String USER_LOGIN_API = "https://sheltered-fjord-8731.herokuapp.com/api/user/token";
     public static final String USER_NAME_CHECK_API = "https://sheltered-fjord-8731.herokuapp.com/api/unique/username";
     public static final String USER_PROFILE_API = "https://sheltered-fjord-8731.herokuapp.com/api/user/info";
     public static final String EVENT_API = "http://sheltered-fjord-8731.herokuapp.com/api/events/";
+    public static final String USER_INFO_EDIT_API = "https://sheltered-fjord-8731.herokuapp.com/api/user/edit";
+
 }
