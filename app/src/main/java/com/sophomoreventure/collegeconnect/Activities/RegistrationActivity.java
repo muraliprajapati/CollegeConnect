b08b38f46c7d1214e4e432e925e77d393ca1a012
murali-prajapati <murali.prajapati555@gmail.com>
2016-02-02 19:26:54 +0530

    Registration bug fixed

--- app/src/main/java/com/sophomoreventure/collegeconnect/Activities/RegistrationActivity.java
+++ app/src/main/java/com/sophomoreventure/collegeconnect/Activities/RegistrationActivity.java
@@ -13,8 +13,6 @@ import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.util.Patterns;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
@@ -185,27 +183,9 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_registration, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
 
     @Override
     public void onClick(View v) {
@@ -375,7 +355,7 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
             missingFields[5] = true;
         }
 
-        if (isSVNITIan && hostelListSpinner.getSelectedItem().toString().equalsIgnoreCase("Choose hostel")) {
+        if (isSVNITIan && hostelListSpinner.getSelectedItem().toString().equalsIgnoreCase("Choose hostel") && isHostelite) {
             Snackbar.make(mRoot, "Choose hostel", Snackbar.LENGTH_LONG)
                     .show();
             missingFields[6] = true;
