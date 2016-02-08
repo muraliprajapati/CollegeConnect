d0b86f90af9906f8a0e115fde0f4e6ef5a11f172
murali-prajapati <murali.prajapati555@gmail.com>
2016-02-08 21:32:55 +0530

    GCM added

---
+++ app/src/main/java/com/sophomoreventure/collegeconnect/GCM/RegistrationConstants.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sophomoreventure.collegeconnect.GCM;
+
+/**
+ * Containing the constants used to share registration states within the application.
+ */
+public class RegistrationConstants {
+
+    /**
+     * Indicates that the token has been sent to the server.
+     */
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    /**
+     * Indicates that the registration with the server is complete.
+     */
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+
+    public static final String TOKEN = "token";
+}
